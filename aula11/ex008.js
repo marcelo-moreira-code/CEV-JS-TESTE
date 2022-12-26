3f46b61525a9937168363f279fed0af7ad27992e
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-26 19:33:04 -0300

    A11
    C simples/composta

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+// Condição Simples
+var vel = 60.50
+console.log(`A velocidade do seu carro é de ${vel} KM/h`)
+if (vel > 60 ) {
+    console.log(`Você ultrapassou o limite de valocidade permitida. MULTADO!`)
+                }
+console.log(`Dirija sempre com cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+// Condição Composta
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}...`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!') 
+} else {
+    console.log('Você é Estrangeiro!')
+}
