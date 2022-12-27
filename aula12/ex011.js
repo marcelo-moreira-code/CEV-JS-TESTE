31ff93e317e517c95d617240065ff1a1e0d40f90
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-27 14:06:43 -0300

    A12

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 70
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16 ) {
+    console.log('Não vota')
+} else if (idade >= 16 && idade < 18 || idade > 67) {
+        console.log('Seu voto é Opcional.')}
+else if (idade >= 18) {
+    console.log('Voto Obrigatório')}
+    
