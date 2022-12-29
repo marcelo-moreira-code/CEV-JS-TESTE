9403c0c213287a5365bed1ca4a99d2f136367ece
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-28 21:30:58 -0300

    A12ex
    ex14 feito!

--- aula12Ex/ex014/script.js
+++ aula12Ex/ex014/script.js
@@ -1,1 +1,21 @@
-window.alert(`Olá !`)
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 1
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        console.log(`Bom dia!`)
+        img.src = 'manha.png'
+        document.body.style.background = '#ffd25f'
+    } else if (hora >= 12 && hora < 18) {
+        console.log(`Boa Tarde!`)
+        img.src = 'tarde.png'
+        document.body.style.background = '#c32800'
+    } else {
+        console.log(`Boa Noite!`)
+        img.src = 'noite.png'
+        document.body.style.background = '#1f1f21'
+    }
+}
