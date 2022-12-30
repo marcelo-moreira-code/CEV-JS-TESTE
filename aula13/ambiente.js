efbd12a84c693873feada9b3b7af6abb96def545
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-29 23:34:20 -0300

    A13
    Do while / while

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+/* Estrutura de Repetição com teste lógico no início
+var cont = 1
+while (cont <= 5) {
+    console.log(`Contando ${cont}`)
+    console.log(`Passo ${cont}`)
+    cont ++  // cont = cont + 1  ou cont ++  ou cont = cont + 1
+}
+*/
+
+// Estrutura de Repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`JavaScript ${c} -> ${c*c}`)
+    c ++
+} while (c <= 5)
+
+
+
+/*
+console.log('Boa noite!')
+console.log('Boa noite!')
+console.log('Boa noite!')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
