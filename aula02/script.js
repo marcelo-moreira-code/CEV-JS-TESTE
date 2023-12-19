85eb0eccfdd4bfb9006d71ab165fbd4e23c3d12a
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2023-12-19 11:16:01 -0400

    Update script.js

---
+++ aula02/script.js
@@ -0,0 +1,7 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb =  "quickly";
+
+const wordBlanks = "Is a" + "   " + "   " + "   " + "       " + "."; 
+console.log({wordBlanks});
